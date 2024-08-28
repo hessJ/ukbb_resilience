bc4645b7e7398c5de0f8a59d6a453278c20e1a4f
Jonathan Hess <hessjo@upstate.edu>
2024-08-28 12:10:57 -0400

    Add files via upload

---
+++ ukbb_resilience_analyses_all.R
@@ -0,0 +1,273 @@
+require(plyr)
+require(data.table)
+require(dplyr)
+
+# load ukbb compiled data 
+ukbb = readRDS("~/Google Drive/mac_storage/Manuscripts/Resilience/scz/scz-bull_special-issue/draft/manuscript_docs/circulate/edits/final_to_submit/revision/ukbb_compiled_data_qc_filtered.Rdata")
+ukbb = data.frame(ukbb)
+
+fit = glm(ICD10_M06.Other.rheumatoid.arthritis ~ ICD10_F20.Schizophrenia + f.21022.0.0_Age.at.recruitment +  f.31.0.0_Sex +
+            risk*resilience + 
+            GenomeWidePC_1 + GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5, data = ukbb, family=binomial())
+summary(fit)$coefficients
+interactions::interact_plot(fit, pred = 'risk', modx = 'resilience', data = ukbb)
+
+
+
+
+# groups based on self-reported ethicity 
+# groupings = c("100","200","300","400")
+counts = table(ukbb$f.21000.0.0)
+groupings = names(counts[counts >= 500])
+
+counts = colSums(ukbb[,grepl("ICD10_F|lupus|crohn|diabetes|asthma|Fibrosis.and.cirrhosis.of.liver", colnames(ukbb))])
+ids = names(counts[counts >= 500])
+
+ids = colnames(ukbb)[grepl("ICD", colnames(ukbb))]
+grab = ukbb[,ids]
+grab = data.frame(id = ukbb$IID, grab)
+colnames(grab) = gsub("ICD10_", "", colnames(grab))
+colnames(grab) = unlist(lapply(strsplit(colnames(grab), "[.]"), function(x) x[[1]]))
+
+require('comorbidity')
+grab$id = factor(grab$id  )
+melted = reshape2::melt(grab, .id='id')
+melted = melted[melted$value == 1, ]
+melted = melted[,colnames(melted) %in% c("id","variable")]
+
+elixhauser <- comorbidity(x = melted, id = "id", code = "variable", map = "elixhauser_icd10_quan", assign0 = FALSE)
+elixhauser$psycho = 0
+
+coscores = score(elixhauser, assign0 = FALSE, weights = 'swiss')
+coscores_unweighted = score(elixhauser, assign0 = FALSE, weights = NULL)
+
+coscores_df = data.frame(IID = elixhauser$id, comorbidity_score = coscores, comorbidity_score_unweighted = coscores_unweighted)
+
+missing_id = ukbb$IID[!ukbb$IID %in% coscores_df$IID]
+missing_df = data.frame(IID = missing_id, comorbidity_score = 0)
+all_scores = ldply(list(coscores_df, missing_df))
+
+ukbb = merge(ukbb, all_scores, by='IID')
+
+sub = ukbb
+
+rm(ukbb);
+
+sub$dx = sub$ICD10_F20.Schizophrenia
+sub_id = sub[!is.na(sub$dx), ]
+
+fit = glm(dx ~ risk*resilience + comorbidity_score + GenomeWidePC_1 + GenomeWidePC_2 + 
+            GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5 +
+            f.21022.0.0_Age.at.recruitment +  f.31.0.0_Sex, data = sub_id, family=binomial())
+
+stats = data.frame(summary(fit)$coefficients)
+stats = data.frame(pred = rownames(stats), stats, ntotal = nrow(sub_id), ncase = sum(sub_id$dx == 1))
+stats$or = exp(stats$Estimate)
+stats$lwr = exp(stats$Estimate - 1.96 * stats$Std..Error)
+stats$upr = exp(stats$Estimate + 1.96 * stats$Std..Error)
+stats
+ 
+icd10_stats_f20 = stats
+
+icd10_stats_f20 = data.frame(response = "ICD10_F20.Schizophrenia",
+                                 pred = icd10_stats_f20$pred,
+                                 estimate = icd10_stats_f20$Estimate,
+                                 se = icd10_stats_f20$Std..Error,
+                                 pval = icd10_stats_f20$Pr...z..,
+                                 n_sample =icd10_stats_f20$ntotal,
+                                n_case = icd10_stats_f20$ncase)
+ 
+# repeat lifetime ICD code analysis using Elixhauser Comorbidity Index as additional covariates
+# ids = ids[!grepl("F20", ids)]
+# stats_list = list();
+# plot_save = list()
+# for(x in 1:length(ids)){
+#   cat("\rVariable...", x, "of",length(ids))
+#   # // non schizophrenia cases
+#   dx = sub[,colnames(sub) %in% ids[[x]]]
+#   sub$dx = dx
+#   sub_id = sub[!is.na(sub$dx), ]
+#   sub_id = sub_id[sub_id$ICD10_F20.Schizophrenia == 0, ]
+#   
+#   fit = glm(dx ~ risk*resilience + GenomeWidePC_1 + GenomeWidePC_2 + 
+#               GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5 +
+#               f.21022.0.0_Age.at.recruitment +  f.31.0.0_Sex, data = sub_id, family=binomial())
+#   
+#   stats = data.frame(summary(fit)$coefficients)
+#   stats = data.frame(pred = rownames(stats), stats, ntotal = nrow(sub_id), ncase = sum(dx == 1))
+#   stats$response = ids[[x]]
+#   stats_list[[x]] = stats
+#   plot_save[[x]] = interactions::interact_plot(fit, pred='risk', modx = 'resilience')$data
+# }
+# res = ldply(stats_list)
+# res = ddply(res, .(pred), transform ,fdr = p.adjust(Pr...z.., 'fdr'))
+# all_res_narrow = res
+# res = res[grepl("risk:resilience", res$pred), ]
+# 
+# sig = res[res$fdr < .05, ]
+ 
+# -- include all subjects, incuding those with scz diagnosis for icd narrow analysis
+# repeat lifetime ICD code analysis using Elixhauser Comorbidity Index as additional covariates
+counts = colSums(sub[,grepl("ICD10_F|lupus|crohn|diabetes|asthma|Fibrosis.and.cirrhosis.of.liver", colnames(sub))])
+ids = names(counts[counts >= 500])
+
+ids = ids[!grepl("F20", ids)]
+stats_list = list();
+plot_save = list()
+for(x in 1:length(ids)){
+  cat("\rVariable...", x, "of",length(ids))
+  # // non schizophrenia cases
+  dx = sub[,colnames(sub) %in% ids[[x]]]
+  sub$dx = dx
+  sub_id = sub[!is.na(sub$dx), ]
+  # sub_id = sub_id[sub_id$ICD10_F20.Schizophrenia == 0, ]
+  
+  fit = glm(dx ~ risk*resilience +  comorbidity_score + ICD10_F20.Schizophrenia + GenomeWidePC_1 + GenomeWidePC_2 + 
+              GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5 +
+              f.21022.0.0_Age.at.recruitment +  f.31.0.0_Sex, data = sub_id, family=binomial())
+  
+  stats = data.frame(summary(fit)$coefficients)
+  stats = data.frame(pred = rownames(stats), stats, ntotal = nrow(sub_id), ncase = sum(dx == 1))
+  stats$response = ids[[x]]
+  stats_list[[x]] = stats
+  plot_save[[x]] = interactions::interact_plot(fit, pred='risk', modx = 'resilience')$data
+}
+res = ldply(stats_list)
+res = ddply(res, .(pred), transform ,fdr = p.adjust(Pr...z.., 'fdr'))
+ddply(res, .(pred), summarize, sum(fdr < .05))
+
+all_res_narrow = res
+ 
+all_res_narrow = data.frame(   response = all_res_narrow$response, 
+                                 pred = all_res_narrow$pred,
+                                 estimate = all_res_narrow$Estimate,
+                                 se = all_res_narrow$Std..Error,
+                                 pval = all_res_narrow$Pr...z..,
+                                 n_sample = all_res_narrow$ntotal,
+                                 n_case = all_res_narrow$ncase)
+ 
+
+
+# == lifetime diagnoses in icd-10 chapters
+counts = colnames(sub)[grepl("ICD10", colnames(sub))]
+icd_no = gsub("ICD10_", "", unique(unlist(lapply(strsplit(counts, "[.]"), function(x) x[[1]]))))
+cat_grab = unique(gsub("\\d+", "", icd_no)) # remove numbers from character string
+
+# plot_int_combined = plot_int_combined[plot_int_combined$ICD10_F20.Schizophrenia == 0, ]
+
+coef_list = list();
+plot_list = list();
+coef_main_list = list()
+for(x in 1:length(cat_grab)){
+  cat("\rAnalyzing category:", x)
+  id_grab = paste0(paste0(paste0(cat_grab[[x]],0, 0:9), collapse="|"), paste0(paste0(cat_grab[[x]], 10:93), collapse="|"), collapse="|")
+  disorders_icd = sub[,grepl(id_grab, colnames(sub))]
+  disorders_icd = rowSums(disorders_icd)
+  disorders_icd[disorders_icd > 1]  = 1
+  table(disorders_icd)
+  
+  fit = glm(disorders_icd ~ resilience*risk +  comorbidity_score + ICD10_F20.Schizophrenia + GenomeWidePC_1 + GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5 +
+              f.31.0.0_Sex + f.21022.0.0_Age.at.recruitment, data = sub, family=binomial())
+  
+  coef = summary(fit)$coefficients
+  coef = data.frame(pred = rownames(coef), coef)
+  coef$n_case = sum(disorders_icd == 1)
+  coef$n_control = sum(disorders_icd == 0)
+  coef$n_total = length(disorders_icd)
+  coef_list[[x]] = coef
+  
+  out = interactions::interact_plot(fit, pred = 'risk', modx = 'resilience')
+  out = data.frame(out$data)
+  plot_list[[x]] = out
+  
+  fit = glm(disorders_icd ~ resilience + risk + comorbidity_score + ICD10_F20.Schizophrenia + GenomeWidePC_1 + GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5 +
+              f.31.0.0_Sex + f.21022.0.0_Age.at.recruitment, data = sub, family=binomial())
+  
+  stats = summary(fit)$coefficients
+  stats = data.frame(pred = rownames(stats), stats, n_case = sum(disorders_icd == 1), n_control = length(disorders_icd), n_total = nrow(sub))
+  stats$name = cat_grab[[x]]
+  coef_main_list[[x]] = stats
+  
+  
+}
+
+names(coef_list) = cat_grab
+res = ldply(coef_list, .id='response')
+res = ddply(res, .(pred), transform, fdr = p.adjust(Pr...z.., 'fdr'))
+ddply(res, .(pred), summarize, sum(fdr < .05))
+all_icd_broad = res
+
+chapters = data.frame(response = cat_grab,
+                      chapter = c("Diseases of the blood and blood-forming organs and certain disorders involving the immune mechanism",
+                                  "Endocrine, nutritional and metabolic diseases",
+                                  "Mental and behavioural disorders",
+                                  "Diseases of the nervous system",
+                                  "Diseases of the circulatory system",
+                                  "Diseases of the respiratory system",
+                                  "Diseases of the digestive system",
+                                  "Diseases of the skin and subcutaneous tissue",
+                                  "Diseases of the musculoskeletal system and connective tissue",
+                                  "Diseases of the genitourinary system",
+                                  "Symptoms, signs and abnormal clinical and laboratory findings, not elsewhere classified",
+                                  "Injury, poisoning and certain other consequences of external causes (S)",
+                                  "Injury, poisoning and certain other consequences of external causes (T)",
+                                  "External causes of morbidity and mortality (V)",
+                                  "External causes of morbidity and mortality (W)",
+                                  "External causes of morbidity and mortality (X)",
+                                  "External causes of morbidity and mortality (Y)"))
+
+ 
+all_icd_broad = merge(chapters, all_icd_broad, by='response')
+all_icd_broad$chapter = gsub("[,]", "/", all_icd_broad$chapter)
+  
+
+all_icd_broad = data.frame(   response = all_icd_broad$chapter, 
+                              pred = all_icd_broad$pred,
+                              estimate = all_icd_broad$Estimate,
+                              se = all_icd_broad$Std..Error,
+                              pval = all_icd_broad$Pr...z..,
+                              n_sample = all_icd_broad$n_total,
+                              n_case = all_icd_broad$n_case)
+
+## education
+grab = sub[!is.na(sub$isced_coding),]
+
+fit2 = lm(RNOmni::RankNorm(isced_coding) ~ ICD10_F20.Schizophrenia + f.21022.0.0_Age.at.recruitment + f.31.0.0_Sex +  risk*resilience +  GenomeWidePC_1 +
+            GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5, data = grab)
+coef_educ = data.frame(summary(fit2)$coefficients)
+coef_educ$n_sample = nrow(grab)
+coef_educ
+ 
+## fluid intelligence
+grab = sub[!is.na(sub$f.20016.0.0_Fluid.intelligence.score), ]
+
+fit = lm(scale(f.20016.0.0_Fluid.intelligence.score) ~ ICD10_F20.Schizophrenia + f.21022.0.0_Age.at.recruitment + f.31.0.0_Sex +  risk*resilience +  GenomeWidePC_1 +
+           GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5, data = grab)
+coef_fluid = data.frame(summary(fit)$coefficients)
+coef_fluid$n_sample = nrow(grab)
+coef_fluid
+ 
+
+coef_fluid = data.frame(pred = rownames(coef_fluid), coef_fluid)
+coef_educ = data.frame(pred = rownames(coef_educ), coef_educ)
+all = ldply(list(intel = coef_fluid, educ = coef_educ))
+all = ddply(all, .(pred), transform, fdr = p.adjust(Pr...t.., 'fdr'))
+ 
+cognitive_stats = data.frame(   response = all$.id, 
+                              pred = all$pred,
+                              estimate = all$Estimate,
+                              se = all$Std..Error,
+                              pval = all$Pr...t..,
+                              n_sample = all$n_sample)
+
+all_stats_in_script = ldply(list(scz = icd10_stats_f20,
+           icd_narrow = all_res_narrow,
+           icd_broad = all_icd_broad,
+           cognitive = cognitive_stats), .id='category')
+all_stats_in_script = all_stats_in_script[grepl("risk|resilience", all_stats_in_script$pred), ]
+all_stats_in_script = ddply(all_stats_in_script, .(pred, category), transform, fdr = p.adjust(pval, 'fdr'))
+ddply(all_stats_in_script, .(category, pred), summarize, sum(fdr < .05))
+
+fwrite(all_stats_in_script, file="~/Google Drive/mac_storage/Manuscripts/Resilience/scz/scz-bull_special-issue/draft/manuscript_docs/circulate/edits/final_to_submit/revision/stats_compile_ukbb-1_resilience_0.2.txt", quote=F,row.names=F, sep="\t")
+
+
---
+++ ukbb_resilience_analysis_all_2.R
@@ -0,0 +1,183 @@
+
+# mental health items
+require(data.table);
+require(plyr);
+require(dplyr)
+
+# load ukbb compiled data 
+ukbb = readRDS("~/Google Drive/mac_storage/Manuscripts/Resilience/scz/scz-bull_special-issue/draft/manuscript_docs/circulate/edits/final_to_submit/revision/ukbb_compiled_data_qc_filtered.Rdata")
+ukbb = data.frame(ukbb)
+
+showcase = data.frame(fread("/Users/jonathanhess/Documents/ukbiobank/phenotypes/Data_Dictionary_Showcase.tsv"))
+showcase = showcase[grepl("mental", ignore.case = T, showcase$Path), ]
+showcase = showcase[grepl("Psychosocial factors", showcase$Path),]
+showcase = showcase[showcase$ValueType %in% "Categorical single",]
+showcase = showcase[grepl("ACE", showcase$Notes), ]
+showcase = showcase[showcase$Participants >= 200e3, ]
+range(showcase$Participants)
+
+showcase$iter = paste0("f.",showcase$FieldID,".0.0")
+showcase = showcase[showcase$iter %in% colnames(ukbb), ]
+iter = showcase$iter
+
+
+codings = data.frame(fread("~/Documents/ukbiobank/phenotypes/Codings.tsv", fill = T))
+codings = codings[codings$Coding %in% showcase$Coding, ]
+
+
+plot_list = list();
+coef_list = list();
+main_coef_list = list();
+for(x in 1:length(iter)){
+  cat("\rProgress:", x)
+  grab = ukbb[,colnames(ukbb) %in% iter[[x]]]
+  ukbb$response = grab
+  grab = ukbb[ukbb$response >= 0, ]
+  grab = grab[!is.na(grab$response), ]
+  levels = length(unique(grab$response))
+  if(levels > 2) next
+  
+  
+  fit  = glm(response ~ resilience*risk + f.21022.0.0_Age.at.recruitment + f.31.0.0_Sex +  ICD10_F20.Schizophrenia + GenomeWidePC_1 + GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5, data = grab, family=binomial())
+  coef = summary(fit)$coefficients
+  coef = data.frame(pred = rownames(coef), coef)
+  coef$n_sample = nrow(grab)
+  coef$response = iter[[x]]
+  coef_list[[x]] = coef
+  rm(grab)
+  
+}
+
+
+
+res = ldply(coef_list)
+res = ddply(res, .(pred), transform, fdr = p.adjust(Pr...z.., 'fdr'))
+
+range(res$n_sample) # number of participants that completed the item in the questionarie
+
+colnames(res)[colnames(res) %in% "response"] ="id"
+label = data.frame(id = paste0("f.",showcase$FieldID,".0.0"), Field = showcase$Field)
+res = merge(label, res, by='id')
+
+mental_health_stats = res
+
+mental_health_stats = data.frame(fieldid = mental_health_stats$id, 
+                                 response = mental_health_stats$Field, 
+                                 pred = mental_health_stats$pred,
+                                 estimate = mental_health_stats$Estimate,
+                                 se = mental_health_stats$Std..Error,
+                                 pval = mental_health_stats$Pr...z..,
+                                 n_sample = mental_health_stats$n_sample)
+
+
+# self-harm or contemplated 
+sub = ukbb[ukbb$ever_contemplated_self_harm >= 0, ]
+sub$response = sub$ever_contemplated_self_harm
+sub$response[sub$response > 1] = 1
+fit = glm(response ~ ICD10_F20.Schizophrenia + f.21022.0.0_Age.at.recruitment + f.31.0.0_Sex +  risk*resilience +  GenomeWidePC_1 +
+            GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5, data = sub, family=binomial())
+coef = summary(fit)$coefficients
+coef_ideation = data.frame(pred = rownames(coef), coef)
+coef_ideation$or = exp(coef_ideation$Estimate)
+coef_ideation$lwr = round(exp(coef_ideation$Estimate - 1.96 * coef_ideation$Std..Error),3)
+coef_ideation$upr = round(exp(coef_ideation$Estimate + 1.96 * coef_ideation$Std..Error),3)
+
+
+sub = ukbb[ukbb$ever_self_harmed >= 0, ]
+sub$response = sub$ever_self_harmed
+sub$response[sub$response > 1] = 1
+fit = glm(response ~ ICD10_F20.Schizophrenia + f.21022.0.0_Age.at.recruitment + f.31.0.0_Sex +  risk*resilience +  GenomeWidePC_1 +
+            GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5, data = sub, family=binomial())
+coef = summary(fit)$coefficients
+coef_harm = data.frame(pred = rownames(coef), coef)
+coef_harm$or = exp(coef_harm$Estimate)
+coef_harm$lwr = round(exp(coef_harm$Estimate - 1.96 * coef_harm$Std..Error),3)
+coef_harm$upr = round(exp(coef_harm$Estimate + 1.96 * coef_harm$Std..Error),3)
+
+
+res = ldply(list(`contemplated` = coef_ideation, `selfharm` = coef_harm), .id='response')
+res = ddply(res, .(pred), transform, fdr = p.adjust(Pr...z.., 'fdr'))
+
+harm_res = res
+
+harm_res = data.frame(response = harm_res$Field, 
+                     pred = harm_res$pred,
+                     estimate = harm_res$Estimate,
+                     se = harm_res$Std..Error,
+                     pval = harm_res$Pr...z..,
+                     n_sample = harm_res$n_sample)
+
+
+# is self-reported ethnicity associated with ICD-10 code for schizophrenia (F20)?
+# fit = glm(ICD10_F20.Schizophrenia ~  factor(f.21000.0.0) +  f.21022.0.0_Age.at.recruitment +  f.31.0.0_Sex, data = sub, family=binomial())
+# car::Anova(fit, type='III')
+
+
+# == run analysis of smri
+ukb_smri = data.frame(fread("~/Documents/ukbiobank/ukb_smri_data.txt", nrows=1))
+iters = colnames(ukb_smri)[-1]
+iters = iters[grepl("\\.2.0", iters)]
+iters = iters[iters %in% colnames(ukbb)]
+
+grab = ukbb[,colnames(ukbb) %in% c(iters, "ICD10_F20.Schizophrenia", "GenomeWidePC_1", "GenomeWidePC_2","GenomeWidePC_3", "GenomeWidePC_4","GenomeWidePC_5","resilience","risk","f.25000.2.0",
+                                   "f.25756.2.0", "f.25757.2.0", "f.25758.2.0", "f.25759.2.0", "f.21002.2.0", "f.21001.2.0", "f.12144.2.0", "f.31.0.0_Sex",
+                                   "f.21003.2.0_Age.when.attended.assessment.centre")]
+grab = grab[!rowSums(is.na(grab))/ncol(grab) > 0.5, ]
+grab = grab[,!colSums(is.na(grab)/nrow(grab) > 0.5)]
+
+
+
+coef_list = list();
+main_coef_list = list();
+for(x in 1:length(iters)){
+  cat("\rProgress:",x)
+  response = grab[,colnames(grab) %in% iters[[x]]]
+  grab$scaled = scale(response)
+  
+  fit = lm(scaled ~ resilience * risk  + GenomeWidePC_1 + GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5 + factor(f.31.0.0_Sex) + f.21003.2.0_Age.when.attended.assessment.centre + 
+             f.25000.2.0 + f.25756.2.0 + f.25757.2.0 + f.25758.2.0 + f.25759.2.0 + f.21002.2.0 + f.21001.2.0 + f.12144.2.0, data = grab)
+  
+  coef = summary(fit)$coefficients
+  coef = data.frame(pred = rownames(coef), coef)
+  coef$n_total = nrow(grab)
+  coef$response = iters[[x]]
+  coef_list[[x]] = coef
+  
+  # main effects w/o interaction
+  fit = lm(scaled ~ resilience + risk   + GenomeWidePC_1 + GenomeWidePC_2 + GenomeWidePC_3 + GenomeWidePC_4 + GenomeWidePC_5 + factor(f.31.0.0_Sex) + f.21003.2.0_Age.when.attended.assessment.centre + 
+             f.25000.2.0 + f.25756.2.0 + f.25757.2.0 + f.25758.2.0 + f.25759.2.0 + f.21002.2.0 + f.21001.2.0 + f.12144.2.0, data = grab)
+  
+  coef = summary(fit)$coefficients
+  coef = data.frame(pred = rownames(coef), coef)
+  coef$n_total = nrow(grab)
+  coef$response = iters[[x]]
+  main_coef_list[[x]] = coef
+}
+
+# summary of models w/ interaction of risk*resilience
+res = ldply(coef_list)
+res = ddply(res, .(pred), transform, fdr = p.adjust(Pr...t.., 'fdr'))
+ddply(res, .(pred), summarize, sum(fdr < .05))
+ddply(res, .(pred), summarize, sum(Pr...t.. < .05))
+
+
+
+# label parcellations
+showcase = data.frame(fread("/Users/jonathanhess/Documents/ukbiobank/phenotypes/Data_Dictionary_Showcase.tsv"))
+showcase$FieldID = paste0("f.",showcase$FieldID, ".2.0")
+showcase = showcase[showcase$FieldID %in% res$response, ]
+label = data.frame(response = showcase$FieldID, name = showcase$Field)
+
+
+res = merge(label, res, by='response')
+
+smri_res = res
+
+smri_res  = data.frame(fieldid = smri_res$response, 
+                      response = smri_res$name, 
+                      pred = smri_res$pred,
+                      estimate = smri_res$Estimate,
+                      se = smri_res$Std..Error,
+                      pval = smri_res$Pr...t..,
+                      n_sample = smri_res$n_total)
+
